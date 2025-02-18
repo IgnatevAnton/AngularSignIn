cdef26cdaed0b24c7bac8372e4368e26f0244f03
B000455 <B000455@MWKS652193>
2025-02-18 13:16:33 +0500

    add component user-profile-bar

---
+++ src/app/presentation/features/user-profile-bar/components/user-profile-bar/user-profile-bar.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-user-profile-bar',
+  standalone: false,
+  
+  templateUrl: './user-profile-bar.component.html',
+  styleUrl: './user-profile-bar.component.scss'
+})
+export class UserProfileBarComponent {
+
+}
