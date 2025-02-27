42d52f7f3537849bda98c027ecfc073defb37fd2
B000455 <B000455@MWKS652193>
2025-02-27 12:55:16 +0500

    change directory user/handlers

--- core/infrastructure/src/lib/repository/user/index.ts
+++ core/infrastructure/src/lib/repository/user/index.ts
@@ -1,8 +1,8 @@
-export { UserCheckHandler } from './hand/UserCheck.handler';
-export { UserLoginHandler } from './hand/UserLogin.handler';
-export { UserLogoutHandler } from './hand/UserLogout.handler';
-export { UserRegistrationHandler } from './hand/UserRegistration.handler';
-export { UserVerifivationHandler } from './hand/UserVerifivation.handler';
+export { UserCheckHandler } from './handlers/UserCheck.handler';
+export { UserLoginHandler } from './handlers/UserLogin.handler';
+export { UserLogoutHandler } from './handlers/UserLogout.handler';
+export { UserRegistrationHandler } from './handlers/UserRegistration.handler';
+export { UserVerifivationHandler } from './handlers/UserVerifivation.handler';
 
 export { MappingUser } from './mapping/MappingUser';
 export { ValidateUser } from './validation/ValidateUser';
